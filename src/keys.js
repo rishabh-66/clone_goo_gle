88d377874fde67b69e66d653be99a846e73122aa
rishabh-66 <58128726+rishabh-66@users.noreply.github.com>
2021-01-18 22:56:49 +0530

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,4 +1,4 @@
-// export const API_KEY = "AIzaSyAsGWQdcqJkLrSkYPCC81qYl-Rf8R8RFTw"
-export const API_KEY = "AIzaSyAnXpd82543oUq_DU8Zv1hjOkTZPowwvtA"
+// export const API_KEY = "AIzaSyAsGWQdcqJkLrSkYPCC81qYl"
+export const API_KEY = "AIzaSyAnXpd82543oUq_"
 
 export default API_KEY;
